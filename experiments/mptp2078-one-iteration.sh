38e458fcc1bbe983ef27b4e10ed5d83fc646d44c
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-05-22 19:18:23 +0200

    .

--- experiments/mptp2078-one-iteration.sh
+++ experiments/mptp2078-one-iteration.sh
@@ -3,13 +3,14 @@ python3 atpboost.py \
 	--conjectures data/MPTP2078/conjectures \
 	--train_deps data/MPTP2078/train_deps \
 	--statements data/MPTP2078/statements \
-    --features data/MPTP2078/features_binary \
-    --chronology data/MPTP2078/chronology \
+	--features data/MPTP2078/features_binary \
+	--chronology data/MPTP2078/chronology \
+	--proving_script prove-old.sh \
 	--ml_models xgboost \
-    --mining 0 \
+	--mining 0 \
 	--n_jobs 70 \
 	--xgb_rounds 5000 \
 	--xgb_knn_prefiltering 0 \
-    --data_dir data/MPTP2078/atpboost_data \
+	--data_dir data/MPTP2078/atpboost_data \
 	--logfile `echo $0 | sed 's/\.sh/.log/g'`
 
