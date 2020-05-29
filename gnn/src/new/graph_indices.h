be2123faffaac68c0acc65a97845d558a95a371d
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-05-29 18:36:03 +0200

    untrack

--- gnn/src/new/graph_indices.h
+++
@@ -1,6 +0,0 @@
-PyObject* py_caml_str(value caml_str);
-PyObject* py_caml_str_list(value l);
-
-PyObject* py_graph_indices(PyObject *, PyObject *);
-PyObject* py_graph_symbols(PyObject *, PyObject *);
-PyObject* py_load_premsel(PyObject *, PyObject *);
