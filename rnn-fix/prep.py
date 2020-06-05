2fa6e0dec6f069a47fd948a8777ad40a128cfab0
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-06-05 12:08:03 +0200

    fix

--- rnn-fix/prep.py
+++ rnn-fix/prep.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys
 sys.path.append('.')
 from utils import read_stms, read_lines, write_lines
 
@@ -6,5 +6,5 @@ from utils import read_stms, read_lines, write_lines
 conjs = read_lines(sys.argv[1])
 stms = read_stms(sys.argv[2], short=True, tokens=True)
 conjs_stms = [stms[c] for c in conjs]
-write_lines(conjs_stms, os.path.join(self.save_dir, 'test.src'))
-write_lines(conjs, os.path.join(self.save_dir, 'test.names'))
+write_lines(conjs_stms, 'rnn-fix/test.src')
+write_lines(conjs, 'rnn-fix/test.names')
