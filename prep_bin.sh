02ba2d67bb2f379892e78e39a1f4c77f5f708bb2
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-06-26 14:00:12 +0200

    better

--- prep_bin.sh
+++ prep_bin.sh
@@ -1,11 +1,15 @@
-rm -rf bin
+rm -rf bin 2> /dev/null
 mkdir bin
 
-source atpboost_casc_env/bin/activate
+rm -rf atpboost_casc_venv 2> /dev/null
+virtualenv atpboost_casc_venv
+source atpboost_casc_venv/bin/activate
+pip install setuptools==44.0.0 # https://github.com/pypa/setuptools/issues/1963
+pip install -r atpboost_casc_venv.txt
 
-rm -rf dist build *.spec
+rm -rf dist build *.spec 2> /dev/null
 pyinstaller atpboost.py
-cp -r atpboost_casc_env/lib/python3.6/site-packages/xgboost dist/atpboost/
+cp -r atpboost_casc_venv/lib/python3.8/site-packages/xgboost dist/atpboost/
 cp -r dist bin
 
 cp utils.py bin
@@ -16,6 +20,7 @@ cp prove* bin
 cp eprover bin
 cp parallel bin
 cp starexec_run bin
+cp starexec_run_test bin
 
 tar czf atpboost_casc.tgz bin
 
