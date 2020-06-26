5bda683e7b320a6b3345fb0ec292f966fe861092
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-06-26 14:11:52 +0200

    .

--- prep_bin.sh
+++ prep_bin.sh
@@ -6,12 +6,11 @@ virtualenv atpboost_casc_venv
 source atpboost_casc_venv/bin/activate
 pip install setuptools==44.0.0 # https://github.com/pypa/setuptools/issues/1963
 pip install -r atpboost_casc_venv.txt
-
 rm -rf dist build *.spec 2> /dev/null
 pyinstaller atpboost.py
 cp -r atpboost_casc_venv/lib/python3.8/site-packages/xgboost dist/atpboost/
-cp -r dist bin
 
+cp -r dist bin
 cp utils.py bin
 cp deps.py bin
 cp prepare_statements.py bin
