9d7b8e80dc036114effba97e35e97885991437b6
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-07-15 14:20:41 +0200

    minor

--- prove-parallel.sh
+++ prove-parallel.sh
@@ -6,4 +6,4 @@ PROVING_SCRIPT=$3
 rm $PROOFS 2> /dev/null
 ./parallel --link \
 	./$PROVING_SCRIPT \
-	::: `cut -d' ' -f1 $IN_OUT` ::: `cut -d' ' -f2 $IN_OUT` ::: $PROOFS 2> aaa
+	::: `cut -d' ' -f1 $IN_OUT` ::: `cut -d' ' -f2 $IN_OUT` ::: $PROOFS 2> prove-parallel.err
