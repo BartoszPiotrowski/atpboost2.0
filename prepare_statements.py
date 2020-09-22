26bd1f235591abd42312f0c5ac16e1f2ac51df66
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-09-22 16:57:12 +0200

    extracting statements

--- prepare_statements.py
+++ prepare_statements.py
@@ -1,17 +1,29 @@
+#!/bin/env python3
 import sys
-from utils import read, read_lines, write_lines, write_line
+import re
+from utils import read_lines
 
-problems = read_lines(sys.argv[1])
+files = sys.argv[1:]
+problems_or_solutions = []
+for f in files:
+    problems_or_solutions.extend(read_lines(f))
+
 statements_names = set()
-for p in problems:
+for p in problems_or_solutions:
     lines = read_lines(p)
+    lines = [l.replace(' ', '') for l in lines]
     lines = [l for l in lines if l and not l[0] in '#%']
     lines = [l for l in lines if l and not 'include(' in l]
-    lines = ''.join(lines).replace(' ', '')
+    lines = [l for l in lines if l and not 'inference(' in l]
+    lines = ''.join(lines)
     lines = lines.replace(').', ').\n').splitlines()
+    lines = [re.sub(",file\('.*\)\)\.$", ").", l) for l in lines]
     for l in lines:
         name = l.split(',')[0].split('(')[1]
         if not name in statements_names:
+            # TODO when the name is repeated, check if the corresponding
+            # formulas are the same
             statements_names.add(name)
+            _name, _, stms_ = l.split(',', 2)
+            l = _name + ',axiom,' + stms_
             print(l)
-
--- preprocess.sh
+++ preprocess.sh
@@ -4,14 +4,15 @@ OUTDIR=$3
 
 TRAIN_DEPS=$OUTDIR/train_deps
 AVAILABLE_PREMS=$OUTDIR/available_prems
+STATEMENTS=$OUTDIR/statements
 
 mkdir -p $OUTDIR
 echo "Extracting available premises from problems listed in $PROBLEMS ..."
-./deps.py $PROBLEMS > $AVAILABLE_PREMS
-echo "Done. Saved to $AVAILABLE_PREMS"
+./deps.py $PROBLEMS > $AVAILABLE_PREMS && echo "Done. Saved to $AVAILABLE_PREMS"
 # TODO save axioms?
 echo "Extracting training dependencies from solutions listed in $SOLUTIONS ..."
-./deps.py $SOLUTIONS > $TRAIN_DEPS
-echo "Done. Saved to $TRAIN_DEPS"
+./deps.py $SOLUTIONS > $TRAIN_DEPS && echo "Done. Saved to $TRAIN_DEPS"
 
+echo "Extracting formulas adn their names from problems and solutions listed in $PROBLEMS and $SOLUTIONS ..."
+./prepare_statements.py $PROBLEMS $SOLUTIONS > $STATEMENTS && echo "Done. Saved to $STATEMENTS"
 
