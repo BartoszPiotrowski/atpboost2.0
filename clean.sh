12dae04a2aeebf53768f0d1137a29c4cd2ac3aa8
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-09-23 19:09:37 +0200

    clean better

--- clean.sh
+++ clean.sh
@@ -5,5 +5,5 @@ rm -rf ._E
 rm -rf tmp/*
 find . -name '*log' | xargs rm
 find . -name '*.out' | xargs rm -rf
-find . -name '*.data' | xargs rm -rf
+find . -name '*.data*' | xargs rm -rf
 find . -name '*.deps' | xargs rm
