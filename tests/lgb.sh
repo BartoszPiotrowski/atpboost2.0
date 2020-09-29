a2dd97596427cf2b2e6e0308b100c1f6d59ad198
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-09-29 12:49:08 +0200

    lgb test 1

---
+++ tests/lgb.sh
@@ -0,0 +1,15 @@
+./atpboost.py \
+	--iterations 3 \
+	--conjectures data/example/conjectures \
+	--train_deps data/example/train_deps \
+	--statements data/example/statements \
+	--features data/example/features_binary \
+	--chronology data/example/chronology \
+	--ml_models lightgbm \
+	--lgb_eta 0.1 \
+	--lgb_rounds 10 \
+	--lgb_knn_prefiltering 10 \
+	--mining 0.1 \
+	--data_dir ${0%.*}.data \
+	--logfile ${0%.*}.log
+
