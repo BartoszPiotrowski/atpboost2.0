1360812328a314cef62cccfb6c2042d9fb3d517b
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-10-06 13:35:52 +0200

    install lightgbm

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ pip3 install tqdm
 pip3 install joblib
 pip3 install sklearn
 pip3 install xgboost
+pip3 install lightgbm
 
 # installing E prover
 wget http://wwwlehre.dhbw-stuttgart.de/~sschulz/WORK/E_DOWNLOAD/V_2.4/E.tgz
