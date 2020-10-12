9bb2b198b14b684cba440bf604b50a8df91455b3
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-10-12 12:15:25 +0200

    this not needed in main branch

--- clean.sh
+++
@@ -1,9 +0,0 @@
-rm -rf __pycache__
-rm -rf E
-rm -rf E.tgz
-rm -rf ._E
-rm -rf tmp/*
-find . -name '*log' | xargs rm
-find . -name '*.out' | xargs rm -rf
-find . -name '*.data*' | xargs rm -rf
-find . -name '*.deps' | xargs rm
