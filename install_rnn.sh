d9a65842ad278fe2fe51d8cab45fd38dffa98c01
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-10-22 17:07:35 +0200

    installation

--- install_rnn.sh
+++ install_rnn.sh
@@ -7,6 +7,8 @@ pip3 install joblib
 pip3 install sklearn
 pip3 install xgboost
 pip3 install lightgbm
+pip3 install torch torchvision
+pip3 install opennmt-py
 
 # installing E prover
 if [ ! -d E ]; then
@@ -18,4 +20,3 @@ if [ ! -d E ]; then
 	cd ..
 	cp E/PROVER/eprover .
 fi
-
