6e3fbdb4e329cfbdfe8a20357e2b77153ad31ee2
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-11-02 13:25:10 +0100

    gnn conda test

---
+++ tests/gnn_conda.sh
@@ -0,0 +1,17 @@
+
+conda activate atpboost_gnn_venv
+./atpboost.py \
+	--iterations 3 \
+	--conjectures data/example/conjectures \
+	--train_deps data/example/train_deps \
+	--statements data/example/statements \
+	--features data/example/features_binary \
+	--chronology data/example/chronology \
+	--ml_models gnn \
+	--gnn_epochs 10 \
+	--mining 0.1 \
+	--n_jobs 1 \
+	--data_dir ${0%.*}.data \
+	--logfile ${0%.*}.log
+
+
